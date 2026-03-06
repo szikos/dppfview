018914e4962379ce9068e555a711dcbe7b05de0d
Stelios Zikos <92376184+szikos@users.noreply.github.com>
2026-03-06 23:44:04 +0200

    Create sw.js

---
+++ demo/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "dpp-viewer-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "styles.css",
+  "app.js",
+  "data.json",
+  "manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (cached) =>
+        cached ||
+        fetch(event.request).catch(() =>
+          caches.match("index.html")
+        )
+    )
+  );
+});
